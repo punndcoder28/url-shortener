99b3e364616ffa4b4b34786eb2bfb620183e610d
punndcoder28 <puneethk.2899@gmail.com>
2023-08-16 22:58:55 +0530

    feat: load database dsn from .env file

--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -2,19 +2,22 @@ package controllers
 
 import (
 	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 
 	models "github.com/punndcoder28/url-shortner/models"
 )
 
-const dsn = "postgresql://puneeth:5RHMSaa5GqZdCjM7uFXVjQ@projects-prod-3242.7s5.cockroachlabs.cloud:26257/url_shortener?sslmode=verify-full"
+var dsn string
 
 /*
 CreateTables to create all the tables in the database
 */
 func CreateTables() {
+	loadEnv()
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
@@ -25,6 +28,15 @@ func CreateTables() {
 	createURLTable(db)
 
 	fmt.Println("Database Migrated")
+}
+
+func loadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		panic("Error loading .env file")
+	}
+
+	dsn = os.Getenv("DATABASE_DSN")
 }
 
 func createURLTable(db *gorm.DB) {
