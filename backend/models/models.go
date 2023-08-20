e2a346a08e50f567a0b5b32b32fae28426752aa4
punndcoder28 <puneethk.2899@gmail.com>
2023-08-20 14:15:07 +0530

    fix(model): updating model to match hashing strategy

--- backend/models/models.go
+++ backend/models/models.go
@@ -5,7 +5,6 @@ import "time"
 type URL struct {
 	ID        uint      `gorm:"primaryKey:autoIncrement"`
 	Hash      string    `gorm:"type:string;unique:not null"`
-	ShortHash string    `gorm:"type:string;unique;not null:index"`
 	URL       string    `gorm:"type:string;not null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
