f938c4c8356c28150c0006c79895bf2ab488ccb5
punndcoder28 <puneethk.2899@gmail.com>
2023-08-20 15:15:01 +0530

    feat: adding handler methods to get and insert url

--- backend/controllers/controller.go
+++ backend/controllers/controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 
 	helpers "github.com/punndcoder28/url-shortner/helpers"
 	models "github.com/punndcoder28/url-shortner/models"
@@ -23,24 +24,43 @@ func CreateTables() {
 	fmt.Println("Database Migrated")
 }
 
-func insertURL(url string) {
+func insertURL(url string) string {
 	db := helpers.ConnectDB()
 
 	hashedURL := helpers.HashURL(url)
 	urlRecord := models.URL{Hash: hashedURL, URL: url}
 	db.Create(&urlRecord)
+
+	return hashedURL
 }
 
-func getURLFromHash(hash string) []byte {
+func getURLFromHash(hash string) string {
 	db := helpers.ConnectDB()
 
 	var url models.URL
 	db.Where("hash = ?", hash).First(&url)
 
-	jsonURL, err := json.Marshal(url)
-	if err != nil {
-		panic(err)
-	}
+	urlString := url.URL
+	return urlString
+}
 
-	return jsonURL
+func GetURL(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	hash := r.URL.Path[1:]
+	url := getURLFromHash(hash)
+
+	http.Redirect(w, r, url, http.StatusSeeOther)
+}
+
+func CreateURL(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var createURLRequest models.CreateURLRequest
+	_ = json.NewDecoder(r.Body).Decode(&createURLRequest)
+
+	hash := insertURL(createURLRequest.URL)
+	url := helpers.CreateTempURLFromHash(hash)
+	response := map[string]interface{}{"url": url}
+	json.NewEncoder(w).Encode(response)
 }
