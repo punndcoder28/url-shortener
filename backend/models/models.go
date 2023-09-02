8e9d48f0d670de1a0fed56e025c32bf1f615f17d
punndcoder28 <puneethk.2899@gmail.com>
2023-09-02 19:39:00 +0530

    fix: updating model to handle null values and throw error

--- backend/models/models.go
+++ backend/models/models.go
@@ -4,8 +4,8 @@ import "time"
 
 type URL struct {
 	ID        uint      `gorm:"primaryKey:autoIncrement"`
-	Hash      string    `gorm:"type:string;unique:not null"`
-	URL       string    `gorm:"type:string;not null"`
+	Hash      string    `gorm:"type:string;unique:not null;default:null"`
+	URL       string    `gorm:"type:string;not null;default:null"`
 	CreatedAt time.Time `gorm:"autoCreateTime"`
 }
 
