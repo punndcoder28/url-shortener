9e6d6b47c01ded7bd007355b95d9b8f864469728
punndcoder28 <puneethk.2899@gmail.com>
2023-09-02 19:04:50 +0530

    feat: adding request body validation for create url handler

---
+++ backend/helpers/validators.go
@@ -0,0 +1,16 @@
+// Package helpers : This package contains all the validator helper functions
+package helpers
+
+import (
+	"github.com/go-playground/validator/v10"
+
+	models "github.com/punndcoder28/url-shortner/models"
+)
+
+var validate *validator.Validate
+
+func ValidateShortenURLRequest(createURLRequest models.CreateURLRequest) error {
+	validate = validator.New(validator.WithRequiredStructEnabled())
+
+	return validate.Struct(createURLRequest)
+}
