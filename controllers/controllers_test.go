171ab0d1d19dd85387cab62d55a478f3c48646ed
punndcoder28 <puneethk.2899@gmail.com>
2023-09-18 23:15:38 +0530

    feat: adding test for duplicate url request body

--- controllers/controllers_test.go
+++ controllers/controllers_test.go
@@ -64,4 +64,31 @@ func TestCreateEmptyURL(t *testing.T) {
 	)
 }
 
+func TestCreateDuplicateURL(t *testing.T) {
+	requestBody := strings.NewReader(`{"url":"https://www.google.com"}`)
+	req := httptest.NewRequest(http.MethodPost, "/api/shorten", requestBody)
+	w := httptest.NewRecorder()
+
+	CreateURL(w, req)
+
+	resp := w.Result()
+
+	assert.Equal(t, http.StatusInternalServerError, resp.StatusCode, "Status code should be 500")
+
+	responseBody, err := io.ReadAll(resp.Body)
+
+	assert.NoError(t, err, "Error should be nil while reading response body")
+
+	responseBody = []byte(strings.TrimSuffix(string(responseBody), "\n"))
+
+	expectedResponseBody := `{"error":"ERROR: duplicate key value violates unique constraint \"urls_hash_key\" (SQLSTATE 23505)"}`
+
+	assert.Equal(
+		t,
+		expectedResponseBody,
+		string(responseBody),
+		"Response body should be equal to expected response body",
+	)
+}
+
 func TestGetURL(t *testing.T) {}
